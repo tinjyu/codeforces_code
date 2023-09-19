fd0d8107de44aaa568cebca7ac3873f344d50ba3
tinjyu <ken282828282828@gmail.com>
2023-09-19 14:23:28 +0800

    Codeforces Round 897 (Div. 2)

--- src/1867/1867C.cpp
+++ src/1867/1867C.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+int s[100005],n;
 int main(){
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>s[i];
+        }
+        int mx=n;
+        for(int i=1;i<=n;i++)
+        {
+            if(s[i]!=i-1)
+            {
+                mx=i-1;
+                break;
+            }
+        }
+        cout<<mx<<endl;
+        int op=0;
+        cin>>op;
+        while(op!=-1)
+        {
+            cout<<op<<endl;
+            cin>>op;
+        }
+        
+    }
     return 0;
 }
